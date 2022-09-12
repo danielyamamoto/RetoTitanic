89d1d84b4e3b08b0a07e4a85d596f379266236b8
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2022-09-12 15:20:18 -0500

    Setup app done

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View className="w-full h-full flex items-center justify-center">
+            <Text>Home</Text>
+        </View>
+    )
+}
